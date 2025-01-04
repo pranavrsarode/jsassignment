5ded7f837687f5a38170d9103e31867d5dd3d004
Pranav Sarode <sarodepranav2001@gmail.com>
2025-01-04 16:14:53 +0530

    Factorial of n number code completed

--- js/script.js
+++ js/script.js
@@ -33,7 +33,61 @@ Print Fibonacci series n numbers,
         return ansArr;
     }
 }
+/*
 console.log(fib(0));
 console.log(fib(1));
 console.log(fib(5));
 console.log(fib(8));
+*/
+
+
+
+/*
+Given a positive integer, n. Find the factorial of n.
+Examples :
+Input: n = 5
+Output: 120
+Explanation: 5*4*3*2*1 = 120
+Input: n = 4
+Output: 24
+Explanation: 4*3*2*1 = 24
+*/
+
+function factorial(n) { 
+    let res = 1;     
+    if(n === 0)
+        return 1;
+    for (let i = 2; i <= n; i++) 
+        res = res * i; 
+    return res; 
+}
+let n = 5; 
+//console.log(factorial(n));
+
+
+
+/*
+Write a function that returns the least common multiple (LCM) of two integers.
+Examples
+lcm(9, 18) ➞ 18
+lcm(8, 5) ➞ 40
+lcm(17, 11) ➞ 187
+*/
+
+function gcd(a, b) 
+{ 
+if (b == 0) 
+    return a; 
+return gcd(b, a % b); 
+} 
+
+// Function to return LCM of two numbers 
+function lcm(a, b) 
+{ 
+    return (a / gcd(a, b)) * b; 
+} 
+
+// Driver program to test above function 
+ 
+let a = 15, b = 20; 
+console.log("LCM of " + a + " and "+ b + " is " + lcm(a, b));
