0e7cf75fae434dc0f960080ee5fc648f126be22d
Pranav Sarode <sarodepranav2001@gmail.com>
2025-01-04 18:40:10 +0530

    Space between instance of lower character followed by upper character

--- js/script.js
+++ js/script.js
@@ -57,49 +57,58 @@ function factorial(n) {
         res = res * i; 
     return res; 
 }
+
 console.log("Print Fibonacci series n numbers");
 console.log(factorial(5));
 console.log(factorial(4));
 
 
+
 /*
-Write a function that returns the least common multiple (LCM) of two integers.
+3. Write a function that returns the least common multiple (LCM) of two integers.
 Examples
 lcm(9, 18) ➞ 18
 lcm(8, 5) ➞ 40
 lcm(17, 11) ➞ 187
 */
+function lcm(num1, num2){
+    let lcm = 1;
+    let multiplesArray = [];
 
-function gcd(a, b) 
-{ 
-if (b == 0) 
-    return a; 
-return gcd(b, a % b); 
-} 
+    if(num2 > num1){
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+    }
+    for(i = 1; i<= num2; i++){
+        multiplesArray.push(num1*i);
+    }
+    for(j = 1; j<= num1; j++){
+        if(multiplesArray.indexOf(num2*j) !== -1){
+            return num2*j;
+        }
+    }
+    return lcm;
+}
+console.log("Least common multiple (LCM) of two integers");
+console.log(lcm(9, 18));
+console.log(lcm(8, 5));
+console.log(lcm(17, 11));
 
-// Function to return LCM of two numbers 
-function lcm(a, b) 
-{ 
-    return (a / gcd(a, b)) * b; 
-} 
-
-// Driver program to test above function 
- 
-let a = 15, b = 20; 
-//console.log("LCM of " + a + " and "+ b + " is " + lcm(a, b));
 
 
 /*
-Write a function that inserts a white space between every instance of a lower character followed immediately by an upper character.
+4. Write a function that inserts a white space between every instance of a lower character followed immediately by an upper character.
 Examples
 insertWhitespace("SheWalksToTheBeach") ➞ "She Walks To The Beach"
 insertWhitespace("MarvinTalksTooMuch") ➞ "Marvin Talks Too Much"
 insertWhitespace("TheGreatestUpsetInHistory") ➞ "The Greatest Upset In History"
 */
 
-function insertSpace(str) {
-    let result = insertSpace("SheWalksToTheBeach");
-    return str.replace(/([a-z])([A-Z])/g, '$1 $2');
-    console.log(result);
+function insertWhitespace(str){
+    return str.replace(/([A-Z])/g," $1").slice(1);
 }
-insertSpace(str);
+console.log("Space between every instance of a lower character followed immediately by an upper character");
+console.log(insertWhitespace("SheWalksToTheBeach"));
+console.log(insertWhitespace("MarvinTalksTooMuch"));
+console.log(insertWhitespace("TheGreatestUpsetInHistory"));
