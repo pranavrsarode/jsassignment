1729cb66bde4c86b6af73d2958d642938bfef841
Pranav Sarode <sarodepranav2001@gmail.com>
2025-01-07 12:00:01 +0530

    Reverse the words which have odd length

--- js/script.js
+++ js/script.js
@@ -273,3 +273,29 @@ console.log(getHashTags("How the Avocado Became the Fruit of the Global Trade"))
 console.log(getHashTags("Why You Will Probably Pay More for Your Christmas Tree This Year"));
 console.log(getHashTags("Hey Parents, Surprise, Fruit Juice Is Not Fruit"));
 
+
+
+/*
+10.Given a string, reverse all the words which have odd length. The even length words are not changed.
+Examples
+reverseOdd("Bananas") ➞ "sananaB"
+reverseOdd("One two three four") ➞ "enO owt eerht four"
+reverseOdd("Make sure uoy only esrever sdrow of ddo length")
+➞ "Make sure you only reverse words of odd length"
+*/
+
+
+function reverseOdd(str) {
+    return str.split(" ").map(word =>
+      word.length % 2 === 1
+        ? word
+            .split("")
+            .reverse()
+            .join(" ")
+        : word
+    ).join(" ");
+}
+console.log("Reverse the words which have odd length");
+console.log(reverseOdd("Bananas"));
+console.log(reverseOdd("One two three four"));
+console.log(reverseOdd("Make sure uoy only esrever sdrow of ddo length"));
