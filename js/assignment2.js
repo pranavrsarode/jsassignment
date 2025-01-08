450c39b3c32adccd7b9ccdd829557f1edacbec91
Pranav Sarode <sarodepranav2001@gmail.com>
2025-01-08 16:01:12 +0530

    return the maximum number of items you could possibly buy

--- js/assignment2.js
+++ js/assignment2.js
@@ -128,3 +128,39 @@ console.log(nearestChapter({
 "Chapter 1a" : 1,
 "Chapter 1b" : 5
 }, 3));
+
+
+
+/*
+4.Souvenir Shop
+On a vacation abroad, you see a souvenir shop and decide to head inside. There are snow globes, postcards, fridge magnets and all the other goodies you'd typically find.
+Given an array of prices of things in the store and the amount of money you have, return the maximum number of items you could possibly buy.
+Examples
+maxItems(["$1", "$1", "$2"], "$3") ➞ 2
+maxItems(["$10", "$7", "$2", "$60"], "$20") ➞ 3
+maxItems(["$15", "$5", "$30", "$30", "$10"], "$2") ➞ "Not enough funds!"
+Notes
+    • If you cannot afford anything, return "Not enough funds!"
+    • Prices will be whole numbers.
+*/
+
+
+function maxItems(itemPrices, budget){
+    budget = parseInt(budget.replace(/[^0-9.-]+/g,""));
+    itemPrices = itemPrices
+        .map(itemPrice => parseInt(itemPrice.replace(/[^0-9.-]+/g,"")))
+        .sort((a, b) => a - b);
+    
+    let affordableItemsCount = 0;
+    for (let index = 0; index < itemPrices.length && budget > 0; index++) {
+        const itemPrice = itemPrices[index];
+        if( budget>0 && (budget-itemPrice>0)){
+            budget -= itemPrice;
+            affordableItemsCount ++;
+        }
+    }
+    return affordableItemsCount?affordableItemsCount:"Not enough funds!";
+}
+console.log(maxItems(["$1", "$1", "$2"], "$3"));
+console.log(maxItems(["$10", "$7", "$2", "$60"], "$20"));
+console.log(maxItems(["$15", "$5", "$30", "$30", "$10"], "$2"));
